a1bd9fcd5a68c70a4dea108bb4ee92c70817effd
BeaulieuLoic <E134756Z@U-INF-J-F222-07.iut-nantes.univ-nantes.prive>
2015-02-18 17:40:56 +0100

    L 17:41

--- HIDE'N'SHARE/src/com/hide_n_share/android/Vue_choix_enveloppe_Activity.java
+++ HIDE'N'SHARE/src/com/hide_n_share/android/Vue_choix_enveloppe_Activity.java
@@ -4,7 +4,6 @@ import java.io.File;
 
 import android.app.Activity;
 import android.app.FragmentManager;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
--- HIDE'N'SHARE/src/com/hide_n_share/android/Vue_devoiler_Activity.java
+++ HIDE'N'SHARE/src/com/hide_n_share/android/Vue_devoiler_Activity.java
@@ -1,7 +1,6 @@
 package com.hide_n_share.android;
 
 import android.app.Activity;
-import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
