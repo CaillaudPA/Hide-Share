ac0116b387c2a3a3fe5866b7509bf74d8d8f8054
coco35700 <jezequel35@gmail.com>
2015-02-18 11:32:16 +0100

    11:32

---
+++ HIDE'N'SHARE/src/com/hide_n_share/android/Vue_chargement_Activity.java
@@ -0,0 +1,20 @@
+package com.hide_n_share.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+public class Vue_chargement_Activity extends Activity{
+
+	private ProgressBar mProgress;
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_chargement);
+    
+        //mProgress = (ProgressBar)findViewById(R.id.);
+    }
+	
+
+}
