edfa96771dffe3c4f3516eb1190c9b90917e8d7b
CaillaudPA <E134892X@U-INF-J-F222-07.iut-nantes.univ-nantes.prive>
2015-02-19 11:57:42 +0100

    hjomk

--- HIDE'N'SHARE/src/com/hide_n_share/android/Vue_chargement_Activity.java
+++ HIDE'N'SHARE/src/com/hide_n_share/android/Vue_chargement_Activity.java
@@ -7,11 +7,35 @@ import android.widget.ProgressBar;
 public class Vue_chargement_Activity extends Activity{
 
 	private ProgressBar mProgress;
+	private int i = 0;
 	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vue_chargement);
+        
+        mProgress = (ProgressBar)findViewById(R.id.progressBar2);
+        
+        new Thread(new Runnable(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				while(i<100){
+					i++;
+					mProgress.setProgress(i);
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+				
+			}
+        	
+        }).start();
     }
 	
 
--- HIDE'N'SHARE/src/com/hide_n_share/android/Vue_devoiler_Activity.java
+++ HIDE'N'SHARE/src/com/hide_n_share/android/Vue_devoiler_Activity.java
@@ -2,6 +2,7 @@ package com.hide_n_share.android;
 
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -16,12 +17,21 @@ public class Vue_devoiler_Activity extends Activity implements OnClickListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.vue_devoile);
 		
-		new PopupErreur().display(this, "test erreur");
+		//new PopupErreur().display(this, "test erreur");
+		selectImage = (Button)findViewById(R.id.boutonVueCacherPhotoAPrendre);
+		selectImage.setOnClickListener(this);
 	
 	}
 	
 	@Override
 	public void onClick(View arg0) {
+		
+		if(arg0.equals(selectImage)){
+			Intent intentTest = new Intent(this, Vue_chargement_Activity.class);
+			startActivity(intentTest);
+			finish();
+		}
+		
 
 	}
 
