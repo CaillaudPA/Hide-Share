1fe5d3ac6f690c5245a69a3ec3992e21fe11b3e7
Pierre-Antoine CAILLAUD <E134892X@U-INF-J-F222-07.iut-nantes.univ-nantes.prive>
2015-02-19 09:22:03 +0100

    jk

--- HIDE_N_SHARE/gen/com/hide_n_share/android/R.java
+++ HIDE_N_SHARE/gen/com/hide_n_share/android/R.java
@@ -35,8 +35,9 @@ public final class R {
         public static final int banniere=0x7f020000;
         public static final int fond=0x7f020001;
         public static final int ic_launcher=0x7f020002;
-        public static final int logoappareilphoto=0x7f020003;
-        public static final int repertoirephoto=0x7f020004;
+        public static final int icongalery=0x7f020003;
+        public static final int logoappareilphoto=0x7f020004;
+        public static final int repertoirephoto=0x7f020005;
     }
     public static final class id {
         public static final int Texte1VueCacher=0x7f09001d;
